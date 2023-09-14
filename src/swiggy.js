c286cb8072fe0407ae1a34dff7426d49e0885a40
Keerthanabas <bkeerthanaeee@gmail.com>
2023-09-14 18:12:21 +0530

    adding image in swiggy.js(14.09.23)

--- src/swiggy.js
+++ src/swiggy.js
@@ -4,6 +4,7 @@ import "./swiggy.css"
 import './App.css';
 import "./Routermenu";
 import "./Routercontact";
+import food from './food.jpg';
 
 
 
@@ -12,27 +13,33 @@ const Swiggy = () => {
     
     return (
        
-        <div className = "App" >
-            
-        <h1><b>Sai Hotel</b></h1>
+        
+        <div className = "swiggy">
+        <center>   
+        <h1 ><b>Sai Hotel</b></h1>
+        <div>
+        <img src = {food} width = "300"  alt = "hotel"/>
+        </div>
         <dt><i><b>About Sai Hotel</b></i></dt>
         <dd>Sai hotel vegetarian restaurant chain based out of Coimbatore ... In 1980, a lodging hotel was opened. Currently the sai group has more than 16 restaurants in Coimbatore and in other cities of Tamil Nadu.</dd>
         
         <p>
             <u>If you Want to order please click order button</u>
           <br></br>
+        </p>
         <div>  
            
         {/* <a href = "https://www.swiggy.com/"target="_blank"rel="noreferrer">order</a> <br></br> */}
         {/* <a href = "menu.js">Menu</a> <br></br>
         <a href = "contact.js">Contact</a> */}
         <div>
-        <b><button onClick={() => {navigate("/menu")}}>Menu</button></b>
+        <button onClick={() => {navigate("/menu")}}>Menu</button>
         <button onClick={() => {navigate("/contact")}}>Contact</button>
         </div>
         </div>
-        </p>
+        </center> 
         </div>
+       
     )
 
 }
